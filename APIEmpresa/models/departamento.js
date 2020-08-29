eeda40a535186ead763617a590ffec71c2eaffe9
JPfstack <juanpedro.jimenez@gmail.com>
2020-08-29 21:12:00 +0200

    Crear nuevamente modelos por error anterior.

---
+++ APIEmpresa/models/departamento.js
@@ -0,0 +1,40 @@
+// *!METODOS DEPARTAMENTOS
+
+//RECUPERAR TODOS LOS DEPARTAMENTOS
+function getAllDptos() {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM empresa.departamento', (error, rows) => {
+            if (error) return reject(error)
+            resolve(rows);
+        });
+    })
+};
+
+
+//CREAR NUEVO DEPARTAMENTO
+function createDpto({ nombre, ciudad }) {
+    return new Promise((resolve, reject) => {
+        db.query('INSERT INTO empresa.departamento (nombre,ciudad) VALUES (?,?)',
+            [nombre, ciudad],
+            (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            })
+    })
+};
+
+
+//ACTUALIZAR DEPARTAMENTO
+function updateDpto({ nombre, ciudad, id }) {
+    return new Promise((resolve, reject) => {
+        db.query('UPDATE empresa.departamento SET nombre=?, ciudad=? WHERE id=?',
+            [nombre, ciudad, id],
+            (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+                console.log(result);
+            })
+    })
+};
+
+module.exports = { getAllDptos, createDpto, updateDpto }
