2ad205da0ea4a5a50462f70394993e9b08542770
JPfstack <juanpedro.jimenez@gmail.com>
2020-08-29 17:45:28 +0200

    Intalación de MySQL y prueba de conexión con db.

---
+++ APIEmpresa/dbConfig.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connect = () => {
+    const pool = mysql.createPool({
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        port: 3306,
+        database: "empresa"
+    });
+    global.db = pool;
+
+
+};
+
+
+
+
+module.exports = { connect }
